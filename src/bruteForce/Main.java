b64e3de4b986afd5434351a2f9301615296dc93c
Luka <luka.pilic5@gmail.com>
2019-11-25 20:09:31 +0100

    git init

---
+++ src/bruteForce/Main.java
@@ -0,0 +1,9 @@
+package bruteForce;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/bruteForce/Thready.java
@@ -0,0 +1,13 @@
+package bruteForce;
+
+public class Thready implements Runnable
+{
+
+    public Thready() {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
